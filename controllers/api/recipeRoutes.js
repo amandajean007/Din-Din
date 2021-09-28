59d19a9dcf38053acf746ae9ebc55d5ac756787c
Amanda McBee <ahanes35@yahoo.com>
2021-09-28 16:04:26 -0600

    Creating a working route

---
+++ controllers/api/recipeRoutes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const { Recipe } = require('../../models');
+
+router.post('/', async (req, res) => {
+  try {
+    const newRecipe = await Recipe.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newRecipe);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const RecipeData = await Recipe.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!RecipeData) {
+      res.status(404).json({ message: 'No recipe found with this id!' });
+      return;
+    }
+
+    res.status(200).json(RecipeData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
