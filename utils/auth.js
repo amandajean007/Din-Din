6dfcdc689e568fd1a72979288142cf959c4fda12
Anastayzia Kerschen <anastayzia.kerschen@gmail.com>
2021-09-30 21:04:34 -0600

    working route to login page

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,8 @@
 const withAuth = (req, res, next) => {
   // If the user is not logged in, redirect the request to the login route
-  if (!req.session.logged_in) {
+  // check later if needed 
+    // can be something else maybe? 
+  if (!req.session.userEmail) {
     res.redirect('/login');
   } else {
     next();
