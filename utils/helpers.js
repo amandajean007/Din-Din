eece42ef35a1c2e0d379bb23f6d3d5e26361c224
Anastayzia Kerschen <anastayzia.kerschen@gmail.com>
2021-10-07 20:44:24 -0600

    got rid of the fav stuff

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,7 @@
 module.exports = {
   format_date: (date) => {
     // Format date as MM/DD/YYYY
-    return date.toLocaleDateString();
+    return date.toLocalDateString();
   },
   format_amount: (amount) => {
     // format large numbers with commas
